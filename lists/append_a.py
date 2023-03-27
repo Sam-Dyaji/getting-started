bca26a140e988a258dcc133ac69deb7fe03d5092
Samuel Dyaji <125928146+Sam-Dyaji@users.noreply.github.com>
2023-03-27 19:19:19 +0200

    Update append_a.py

--- lists/append_a.py
+++ lists/append_a.py
@@ -1,5 +1,3 @@
 animals=["koal", "pand", "zebr", "anacond", "bo", "chinchill", "cobr", "gorill", "hyen", "hydr", "iguan", "impal", "pum", "tarantul", "piranh"]
-#animals.append("a")
-new_animals= ["koala", "panda", "zebra", "anaconda", "boa", "chinchilla", "cobra",
-               "gorilla", "hyena", "hydra", "iguana", "impala", "puma", "tarantula", "piranha"]
-print(new_animals)
+animals= ["koala", "panda", "zebra", "anaconda", "boa", "chinchilla", "cobra","gorilla", "hyena", "hydra", "iguana", "impala", "puma", "tarantula", "piranha"]
+print(animals)
